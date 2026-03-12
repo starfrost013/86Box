920d28848e725b49d6229ca7695325ad400bd6f3
starfrost013 <mario64crashed@gmail.com>
2026-03-12 17:18:08 +0000

    add ref2header nv1 header

---
+++ src/video/nv/nv1/nv1.h
@@ -0,0 +1,21 @@
+/*
+ * 86Box    A hypervisor and IBM PC system emulator that specializes in
+ *          running old operating systems and software designed for IBM
+ *          PC systems and compatibles from 1981 through fairly recent
+ *          system designs based on the PCI bus.
+ *
+ *          This file is part of the 86Box distribution.
+ *
+ *          WELCOME TO NVIDIA DRIVER VERSION 2.0! 
+ *
+ * Authors: starfrost
+ *
+ *          Copyright 2024-2026 starfrost
+ */
+
+#pragma once
+
+typedef struct nv1_s
+{
+
+} nv1_t;
